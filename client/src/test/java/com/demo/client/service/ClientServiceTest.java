bc03beb2f6ba2a33d0cf486ba5decd67e6d09c4f
pdbhorania <pdbhorania@gmail.com>
2021-06-03 20:12:43 +0530

    update service test
    added negative scenarios

--- client/src/test/java/com/demo/client/service/ClientServiceTest.java
+++ client/src/test/java/com/demo/client/service/ClientServiceTest.java
@@ -1,12 +1,15 @@
 package com.demo.client.service;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -21,35 +24,68 @@ import com.demo.client.entity.Client;
 import com.demo.client.repository.ClientRepository;
 import com.demo.client.serviceImpl.ClientServiceImpl;
 import com.demo.client.util.ClientUtil;
+import com.demo.client.util.Constants;
 
 @RunWith(PowerMockRunner.class)
 public class ClientServiceTest {
-	
+
 	@InjectMocks
 	private ClientServiceImpl clientService;
-	
+
 	@Mock
 	private ClientRepository clientRepository;
-	
+
 	@Mock
 	private ClientConverter clientConverter;
-	
-	@Mock
-	private ClientUtil clientUtil;
-	
-	
+
+	@Spy
+	private ClientUtil clientUtil = new ClientUtil();
+
 	@Test
 	public void addClientTest() {
 		ClientDto clientDto = getClientDto();
-		Client client = new Client();
-		Mockito.when(clientRepository.save(client)).thenReturn(Mockito.any());
-		Mockito.doNothing().when(clientUtil).validateIdNumber(clientDto.getIdNumber());
-		Mockito.when(clientRepository.findByMobileNumber(clientDto.getMobileNumber())).
-		String response= clientService.addClient(clientDto);
+		Client client = getClient();
+		Mockito.when(clientRepository.save(client)).thenReturn(null);
+		Optional<Client> nullClient = Optional.ofNullable(null);
+		Mockito.when(clientRepository.findById(clientDto.getIdNumber())).thenReturn(nullClient);
+		Mockito.when(clientRepository.findByMobileNumber(clientDto.getMobileNumber())).thenReturn(null);
+		Mockito.when(clientConverter.convertDtoToEntity(clientDto)).thenReturn(client);
+		String response = clientService.addClient(clientDto);
 		assertNotNull(response);
-		Mockito.verify(clientRepository,Mockito.times(1)).save(client);
+		assertEquals(Constants.CREATE_SUCCESS, response);
+		InOrder order = Mockito.inOrder(clientRepository, clientUtil, clientConverter);
+
+		order.verify(clientRepository, Mockito.times(1)).findByMobileNumber(clientDto.getMobileNumber());
+		order.verify(clientRepository, Mockito.times(1)).findById(clientDto.getIdNumber());
+		order.verify(clientConverter, Mockito.times(1)).convertDtoToEntity(clientDto);
+		order.verify(clientRepository, Mockito.times(1)).save(client);
 	}
-	
+
+	@Test(expected = RuntimeException.class)
+	public void addClientTest_InvalidIdLength() {
+		ClientDto clientDto = getClientDto();
+		clientDto.setIdNumber("123");
+		clientService.addClient(clientDto);
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void addClientTest_MobileNumberAlreadyExist() {
+		ClientDto clientDto = getClientDto();
+		Client client = getClient();
+		Mockito.when(clientRepository.findByMobileNumber(clientDto.getMobileNumber())).thenReturn(client);
+		clientService.addClient(clientDto);
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void addClientTest_IDAlreadyExist() {
+		ClientDto clientDto = getClientDto();
+		Client client = getClient();
+		Optional<Client> nullClient = Optional.ofNullable(client);
+		Mockito.when(clientRepository.findById(clientDto.getIdNumber())).thenReturn(nullClient);
+		Mockito.when(clientRepository.findByMobileNumber(clientDto.getMobileNumber())).thenReturn(null);
+		clientService.addClient(clientDto);
+	}
+
 	private ClientDto getClientDto() {
 		ClientDto clientDto = new ClientDto();
 		clientDto.setFirstName("Exazit");
@@ -59,23 +95,17 @@ public class ClientServiceTest {
 		clientDto.setPhysicalAddress("Mumbai");
 		return clientDto;
 	}
-	
-	@Test
-	public void updateClientTest() {
+
+	private Client getClient() {
 		Client client = new Client();
-		Mockito.when(clientRepository.save(client)).thenReturn(Mockito.any());
-		Client response= clientRepository.save(client);
-		assertNotNull(response);
-		Mockito.verify(clientRepository,Mockito.times(1)).save(client);
+		client.setFirstName("Exazit");
+		client.setLastName("Exazit");
+		client.setIdNumber("9201010001000");
+		client.setMobileNumber(99999999999L);
+		client.setPhysicalAddress("Mumbai");
+		return client;
 	}
-	
-	@Test
-	public void searchClientTest() {
-		Client client = new Client();
-		Mockito.when(clientRepository.findByFirstName(client.getFirstName())).thenReturn(Mockito.anyList());
-		List<Client> response= clientRepository.findByFirstName(client.getFirstName());
-		assertNotNull(response);
-		Mockito.verify(clientRepository,Mockito.times(1)).findByFirstName(client.getFirstName());
-	}
+
+//likewise we can have update and search test cases
 
 }
